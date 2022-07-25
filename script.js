be83a5aa4a1499438c282e342e8a6606660b1c9d
Ayelen Aguilar <aye47aguilar@gmail.com>
2022-07-25 17:10:06 -0300

    algoritmo usando un ciclo

---
+++ script.js
@@ -0,0 +1,8 @@
+/* asignar nro de mesa a invitados ingresando sus nombres*/
+
+for (let i =1;i<16;i++){
+    let Invitado= prompt("Ingresar un nombre").toLowerCase();
+
+    console.log( "Mesa nro:"+ i + "Invitado :"+ Invitado);
+}
+
