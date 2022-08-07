b91e8925293a49f5c68b0aac8eae50d5de055354
Ayelen Aguilar <aye47aguilar@gmail.com>
2022-08-06 21:00:53 -0300

    DOM

--- script.js
+++ script.js
@@ -1,40 +1,18 @@
 // RESERVA DE LUGAR EN LA FILA VIRTUAL PARA LA VENTA DE ENTRADAS DE UN CONCIERTO
 
-function entradas(){
 
-    let precios
-    do {
-        precios= prompt('Elija una opción para ver los precios + gastos por servicio o reservar:\n\n 1.Campo vip \n 2.Campo trasero \n 3.Platea general \n 4. Reservar lugar en la fila').toLowerCase()
-    
-        if(precios==1){
-            alert("CAMPO VIP $15000 + Gastos por servicio($1000)")
-        }
-        else if(precios==2){
-            alert("CAMPO TRASERO $12500+ Gastos por servicio ($800)")
-        }
-        else if(precios==3){
-            alert("PLATEA GENERAL $9000+ Gastos por servicio ($500)")
-        }
-        else if(precios==4){
-            break
-        }else{
-            alert("Ingrese una de las opciones")
-        }
-        
-    } while (precios !=4);
+const opciones= document.getElementById('opciones')
 
-}
 
-entradas()
+opciones.innerHTML=` <div class="card"><p id="campoVip">CAMPO VIP</p>
+<button class="boton">Ver precios</button></div>
+<div class="card"><p id="campoTrasero">CAMPO TRASERO</p>
+<button class="boton">Ver precios</button></div>
+<div class="card" ><p  id="platea">PLATEA GENERAL</p>
+<button class="boton">Ver precios</button></div>
+<div class="card"><p  id="reservar">RESERVAR LUGAR EN LA FILA</p>
+<button class="boton">Reservar lugar en fila</button> </div>`
 
-let nombre= true
-while(nombre){
-    let nombre= prompt("Ingrese nombre y apellido").toLowerCase()
-    let email= prompt("Ingrese un email").toLowerCase()
-
-    alert(`Hola ${nombre}! \n En el lapso de 42hs te va a estar llegando un mail a ${email} con tu nro de lugar en la fila virtual para el dia de la venta oficial, gracias por elegirnos!`)
-    break
-}
 
 
 class datos{
@@ -64,7 +42,7 @@ const precioEntradas=[
 const buscar= precioEntradas.find((precioEntradas)=> precioEntradas.sector=== "PLATEA GENERAL")
 const buscar2= precioEntradas.find((precioEntradas)=> precioEntradas.sector=== "CAMPO TRASERO")
 console.log(buscar)
-console.log(buscar2)
+console.log (buscar2)
 
 
 
